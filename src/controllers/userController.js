40ac3e99b9980cfad7cbf1d5744036358b6e51a1
Jessé Toledo <jesse_hacker@hotmail.com>
2021-08-31 16:58:24 -0300

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,15 +24,40 @@ module.exports = {
   async create(req, res) {
     const { name, email, password } = req.body;
     try {
-      let user = await UserModel.findOne({ email });
+      const user = await UserModel.findOne({ email });
       if (!user) {
-        let data = { name, email, password };
+        const data = { name, email, password };
         user = await UserModel.create(data);
         user.password = undefined;
         return res.status(200).json(user);
       } else {
         return res.status(400).json(user);
       }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send(error);
+    }
+  },
+  async delete(req, res) {
+    const { id } = req.params;
+    try {
+      const user = await UserModel.findByIdAndDelete({ _id: id });
+      if (!user) return res.status(404).send(id);
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send(error);
+    }
+  },
+  async update(req, res) {
+    const { id, name, email, password } = req.body;
+    const data = { name, email, password };
+    try {
+      const user = await UserModel.findOneAndUpdate({ _id: id }, data, {
+        new: true,
+      });
+      if (!user) return res.status(404).send(id);
+      res.json(user);
     } catch (error) {
       console.error(error);
       return res.status(500).send(error);
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,7 @@ const UserController = require("./controllers/userController");
 routes.get("/api/users/:id", UserController.details);
 routes.get("/api/users", UserController.index);
 routes.post("/api/users", UserController.create);
+routes.delete("/api/users/:id", UserController.delete);
+routes.put('/api/users',UserController.update);
 
 module.exports = routes;
